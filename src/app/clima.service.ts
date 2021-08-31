3fdeb62f7835f98afd0cf89f59bf47d06595eca0
AleKurlat <79816198+AleKurlat@users.noreply.github.com>
2021-08-30 23:54:14 -0300

    comentarios

--- src/app/clima.service.ts
+++ src/app/clima.service.ts
@@ -33,14 +33,15 @@ export class ClimaService {
       (this.url, reqBody)
       .pipe(
         map((respuesta) => {
-          if (respuesta.registros === null) {
-            return { ...respuesta, registros: null } // si no hay historial incluido en la respuesta, la devuelvo tal como vino (la aclaración registros:null es un requisito de TypeScript)
-          } else { // si la respuesta viene con historial, tengo que transformar los registros porque vienen de la base de datos en formato string
-            const registros = respuesta.registros.map(
-              (filaHistorial) => {
-                return { ...filaHistorial, registro: JSON.parse(filaHistorial.registro) }
-              })
+          if (respuesta.registros) {
+            // si la respuesta viene con historial, tengo que transformar los registros porque vienen de la base de datos en formato string
+            const registros = respuesta.registros.map((filaHistorial) => {
+              return { ...filaHistorial, registro: JSON.parse(filaHistorial.registro) }
+            })
             return { ...respuesta, registros }
+          } else {
+            // si no hay historial incluido en la respuesta, la devuelvo tal como vino (la aclaración registros:null es un requisito de TypeScript)
+            return { ...respuesta, registros: null }
           }
         }),
         catchError(this.handleError)
